1c6c62b2c6dd7020a2122189beda9084dd5b26dd
gfdg <dsfgsdfg>
2021-04-16 15:20:18 -0500

    Create an early stopping if the min loss stop decreasing in value

--- working_getting_started_with_tensorflow_keras_for_machine_translation.py
+++ working_getting_started_with_tensorflow_keras_for_machine_translation.py
@@ -22,6 +22,7 @@ import manipulateFolder
 import zipfile
 from tensorflow import keras
 from tensorflow.keras.callbacks import Callback, History, CSVLogger
+import numpy as np
 NAME =""
 
 """# Data"""
@@ -183,6 +184,49 @@ print(model.summary())
 def name_model(epochs,type_of_model,learning_rate,loss_function):
     return f' model is {type_of_model} the epochs {epochs}  learning_rate {learning_rate} ' \
            f'lost function is {loss_function}'
+#Early stop in keras
+class EarlyStoppingAtMinLoss(keras.callbacks.Callback):
+    """Stop training when the loss is at its min, i.e. the loss stops decreasing.
+
+  Arguments:
+      patience: Number of epochs to wait after min has been hit. After this
+      number of no improvement, training stops.
+  """
+
+    def __init__(self, patience=0):
+        super(EarlyStoppingAtMinLoss, self).__init__()
+        self.patience = patience
+        # best_weights to store the weights at which the minimum loss occurs.
+        self.best_weights = None
+
+    def on_train_begin(self, logs=None):
+        # The number of epoch it has waited when loss is no longer minimum.
+        self.wait = 0
+        # The epoch the training stops at.
+        self.stopped_epoch = 0
+        # Initialize the best as infinity.
+        self.best = np.Inf
+
+    def on_epoch_end(self, epoch, logs=None):
+        current = logs.get("loss")
+        if np.less(current, self.best):
+            self.best = current
+            self.wait = 0
+            # Record the best weights if current results is better (less).
+            self.best_weights = self.model.get_weights()
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch
+                self.model.stop_training = True
+                print("Restoring model weights from the end of the best epoch.")
+                self.model.set_weights(self.best_weights)
+
+    def on_train_end(self, logs=None):
+        if self.stopped_epoch > 0:
+            print("Epoch %05d: early stopping" % (self.stopped_epoch + 1))
+
+
 """# Hyperparameters"""
 
 learning_rate = Constant.LEARNING_RATE # 0.0001, 0.00xxx1
@@ -206,7 +250,7 @@ logs = Callback()
 csv_logger = CSVLogger(csv_Name)
 tensorboard  = TensorBoard(log_dir="logs/{}".format(NAME))
 epochs = Constant.EPOCHS
-model.fit(train_dataset, validation_data = val_dataset, epochs = epochs, verbose = 1, callbacks=[history,csv_logger,tensorboard])
+model.fit(train_dataset, validation_data = val_dataset, epochs = epochs, verbose = 1, callbacks=[history,csv_logger,tensorboard,EarlyStoppingAtMinLoss()])
 
 
 """## Save model"""
